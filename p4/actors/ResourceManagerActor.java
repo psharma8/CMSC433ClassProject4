80080d5f47bc589071e5ffb796e483038a32c121
psharma8 <31544890+psharma8@users.noreply.github.com>
2019-11-27 02:38:46 -0500

    Add files via upload

---
+++ p4/actors/ResourceManagerActor.java
@@ -0,0 +1,73 @@
+package cmsc433.p4.actors;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import cmsc433.p4.enums.*;
+import cmsc433.p4.messages.*;
+import cmsc433.p4.util.*;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+
+public class ResourceManagerActor extends UntypedActor {
+	
+	private ActorRef logger;					// Actor to send logging messages to
+	
+	/**
+	 * Props structure-generator for this class.
+	 * @return  Props structure
+	 */
+	static Props props (ActorRef logger) {
+		return Props.create(ResourceManagerActor.class, logger);
+	}
+	
+	/**
+	 * Factory method for creating resource managers
+	 * @param logger			Actor to send logging messages to
+	 * @param system			Actor system in which manager will execute
+	 * @return					Reference to new manager
+	 */
+	public static ActorRef makeResourceManager (ActorRef logger, ActorSystem system) {
+		ActorRef newManager = system.actorOf(props(logger));
+		return newManager;
+	}
+	
+	/**
+	 * Sends a message to the Logger Actor
+	 * @param msg The message to be sent to the logger
+	 */
+	public void log (LogMsg msg) {
+		logger.tell(msg, getSelf());
+	}
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param logger			Actor to send logging messages to
+	 */
+	private ResourceManagerActor(ActorRef logger) {
+		super();
+		this.logger = logger;
+	}
+
+	// You may want to add data structures for managing local resources and users, storing
+	// remote managers, etc.
+	//
+	// REMEMBER:  YOU ARE NOT ALLOWED TO CREATE MUTABLE DATA STRUCTURES THAT ARE SHARED BY
+	// MULTIPLE ACTORS!
+	
+	/* (non-Javadoc)
+	 * 
+	 * You must provide an implementation of the onReceive() method below.
+	 * 
+	 * @see akka.actor.UntypedActor#onReceive(java.lang.Object)
+	 */
+	@Override
+	public void onReceive(Object msg) throws Exception {		
+		
+	}
+}
